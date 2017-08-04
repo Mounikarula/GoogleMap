0d1fd2c36cb3007108e2977d58a6a4fd50215f87
mounika <mounika.rgula@gmail.com>
2017-08-04 15:32:50 -0500

    modifications made to javascript file to add a marker to the map

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 	function initMap() {
 		var options ={
 			zoom:8,
-			center: {lat:31.9686, lng:-99.9018}
+			center: {lat:41.9686, lng:-79.9018}
 		}
 
 		var map = new google.maps.Map(document.getElementById('map'), options);
+
+		var marker = new google.maps.Marker({
+			position: {lat:42.4668, lng:-79.9495},
+			map:map
+		})
 	}
