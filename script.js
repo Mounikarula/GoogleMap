ffe62e7062f5e7bc2011a3bbaaaab055ea0ea586
mounika <mounika.rgula@gmail.com>
2017-08-08 10:06:30 -0500

    added properties into an array of objects

--- script.js
+++ script.js
@@ -25,14 +25,14 @@
 	addMarker({
 		coords:{lat:41.9686, lng:-79.9018},
         iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        content: '<h1>Beautiful Place<h1>'
+        content: '<h1>Beautiful lake<h1>'
               });
 	addMarker({coords:{lat:40.4406, lng:-79.9959}});
 	addMarker({coords:{lat:41.4993, lng:-81.6944}});
 	addMarker({
 		coords:{lat:39.9612, lng:-82.9988},
         iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        content: '<h1>Beautiful Place<h1>'  
+        content: '<h1>Beautiful place<h1>'  
               });
 
 	// function to add multiple markers to the map
